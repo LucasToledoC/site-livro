4fe9960d25e23fcbd335cc220877d0360edbe53c
Lucas Toledo <lucastoledocort25@gmail.com>
2025-09-17 21:26:37 -0300

    arrumando deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react( )],
-  base: '/site-livro/', // <--- IMPORTANTE: Substitua 'hora-da-estrela-site' pelo NOME EXATO do seu repositório no GitHub
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
